8394221c67ae77eba18591ed9a591ae2040439ec
m-ogino <ogi.fordevelopment@gmail.com>
2022-02-22 23:15:38 +0900

    add some

---
+++ chat/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+	"github.com/go_apps/trace"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"sync"
+	"text/template"
+)
+
+// templ represents a single template
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// ServeHTTP handles the HTTP request.
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, r)
+}
+
+func main() {
+	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
+	flag.Parse()
+	r := newRoom()
+	r.tracer = trace.New(os.Stdout)
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+	go r.run()
+	log.Println("Webserver start. port: ", *addr)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
---
+++ trace/tracer_test.go
@@ -0,0 +1,19 @@
+package trace
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	var buf bytes.Buffer
+	tracer := New(&buf)
+	if tracer == nil {
+		t.Errorf("Newからの戻り値がnilです")
+	} else {
+		tracer.Trace("こんにちは、traceパッケージ")
+		if buf.String() != "こんにちは、traceパッケージ\n" {
+			t.Errorf("'%s'という誤った文字列が出力されました", buf.String())
+		}
+	}
+}
